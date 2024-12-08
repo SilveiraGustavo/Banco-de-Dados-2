a5e0ea5e45c6766862b52cb131b3a2254176142f
Gustavo Silveira <113398159+SilveiraGustavo@users.noreply.github.com>
2024-12-07 21:53:22 -0300

    Start of the project

---
+++ Btree.py
@@ -0,0 +1,13 @@
+# Trabalho Final da disciplina de Banco de Dados 2
+# Professor: Marcos Roberto Ribeiro
+# Aluno: Gustavo Silveira Dias 
+
+
+import sys
+import csv
+import numpy as np
+import time
+
+
+size_page =  4096 # Definindo o tamanho da página em Bytes
+file = "arquivo" # Definindo o arquivo que vai ser utilizado 
---
+++ Extensivel.py
@@ -0,0 +1,11 @@
+# Trabalho Final da disciplina de Banco de Dados 2
+# Professor: Marcos Roberto Ribeiro
+# Aluno: Gustavo Silveira Dias 
+
+# Implementação do Hash Extensível 
+
+import sys
+import csv
+import numpy as np
+import time
+
