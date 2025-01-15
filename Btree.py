fc13429f8a066f4ec8d66c126e09a9a20db857ce
Gustavo Silveira <113398159+SilveiraGustavo@users.noreply.github.com>
2025-01-15 16:57:50 -0300

    refactor
    Refatoração completa do projeto Btree, aplicando conceitos de orientação a objetos para melhorar a legibilidade e organização do código.

--- Btree.py
+++ Btree.py
@@ -1,266 +1,245 @@
-# Trabalho Final da disciplina de Banco de Dados 2
-# Professor: Marcos Roberto Ribeiro
-# Aluno: Gustavo Silveira Dias 
-
-
 import sys
 import csv
-import numpy as np
 import time
 import argparse
 
-Time = time.time()
+PAGE_SIZE_DEFAULT = 4096  # change page size here
+FILE_DEFAULT = "arquivo1.csv"  # change test file here
 
-size_page =  4096 # Definindo o tamanho da página em Bytes
-file = "arquivo1.csv" # Definindo o arquivo que vai ser utilizado 
+class Record:
+    def __init__(self, fields=None):
+        if fields is None:
+            fields = [[], []]
+        self.key = int(fields[0])
+        self.record_fields = fields[1:]
 
-def create_record(Fields=None):
-    if Fields is None:
-        Fields = [[], []]
-    Key = int(Fields[0])
-    Fields_records = Fields[1:]
-    return {'getKey': lambda: Key, 'record': Fields_records}
+    def get_key(self):
+        return self.key
 
-def size_record(record):
-    Key_size = sys.getsizeof(record[0])
-    Fields_size = sum(sys.getsizeof(x) for x in record[1])
-    return Key_size + Fields_size
+    def size(self):
+        key_size = sys.getsizeof(self.key)
+        fields_size = sum(sys.getsizeof(x) for x in self.record_fields)
+        return key_size + fields_size
 
-def format_record(record):
-    return " {k}:{c}".format(k=record[0], c=record[1])
+    def format(self):
+        return "{k}:{c}".format(k=self.key, c=self.record_fields)
 
-def get_key(record):
-    return record[0]
-
-def get_record(record):
-    return record
+    def get_record(self):
+        return self.key, self.record_fields
 
 
-def create_page(sheet=False, size_page=4096):
-    return {
-        'sheet' : sheet,
-        'size_page' : size_page,
-        'record' : [],
-        'key' : [],
-        'children' : [],
-        'page_right' : None,
-        'page_left' : None
-    }
+class Page:
+    def __init__(self, leaf=False, page_size=4096):
+        self.leaf = leaf
+        self.page_size = page_size
+        self.records = []
+        self.keys = []
+        self.children = []
+        self.right_sibling = None
+        self.left_sibling = None
 
-def sheet(page): 
-    return page['sheet']
+    def is_leaf(self):
+        return self.leaf
 
-def size_page(page):
-    if sheet(page):
-        return sum (sys.getsizeof(x) for x in page['record'])
-    else:
-        return sys.getsizeof(page['children']) + sum(sys.getsizeof(x) for x in page['key'])
-
-def is_empty(page):
-    if sheet(page):
-        return not page['record']
-    else: 
-        return not page['key']
-
-def is_full(page):
-    if not is_empty(page):
-        if sheet(page):
-            return (page['size_page'] -  size_page(page)) < sys.getsizeof(page['record'][0])
-        else: 
-            free = page['size_page'] -size_page(page)
-            return free <= (sys.getsizeof (page['key']) / len (page['key']) +
-                            sys.getsizeof (page['children']) / len (page['children']))
-
-def search_children(page, key):
-    if not sheet(page):
-        pos = -1
-        for x in page['key']:
-            pos += 1
-            if key < x:
-                return page['children'][pos]
-        return page['children'][pos + 1]
-    else: 
-        return None
-
-def insert_page(page, element):
-    if sheet (page):
-        if not is_full(page):
-            page['record'].append(element)
-            page['record'].sort(key=lambda x: x.getKey())
-
-def minimum_occupancy(page):
-    return (size_page(page) / page['size_page']) >= 0.50
-
-def to_lan(page):
-    if sheet(page):
-        return ((size_page(page) - sys.getsizeof(page['record'] [0])) / page['size_page']) >= 0.50
-    else:
-        return ((size_page(page) - sys.getsizeof(page['key'])) / page['size_page']) >= 0.50
-
-def search_no(pag, k):
-    if sheet(pag):
-        return pag
-    return search_no(search_children(pag,k),k)
-
-def search_interval(tree, k1,k2):
-    pag = search_no(tree['source'], k1)
-    out_of_range = False
-    vet_interval = []
-
-    while not out_of_range:
-        for x in pag['record']:
-            if 'getKey' in x and k1 <= x['getKey']() <= k2:
-                vet_interval.append(x['getkey']())
-       
-        if not out_of_range:
-            pag = pag['page_right']
-        if pag is None:
-            break
-    return vet_interval
-
-def serach_record(tree, k):
-    pag = search_no(tree['source'], k)
-    reg = None
-
-    for x in pag['record']:
-        if 'getKey' in x and x['getKey']() == k:
-            reg = x
-    return reg, pag
-
-def insert_sheet(N, element, size_page):
-    if len(N['record']) >= size_page:
-        M = {'sheet': True, 'size_page': size_page}
-        M['record'] = N['record'][(len(N['record']) // 2):]
-        N['record'] = N['record'][:(len(N['record']) // 2)]
-        M['page_right'] = None
-        M['page_left'] = None
-        KM = M['record'][0]['getKey']()
-
-        if element['record'][0]['getKey']() < KM:
-            N['record'].append(element)
+    def page_size(self):
+        if self.is_leaf():
+            return sum(sys.getsizeof(x) for x in self.records)
         else:
-            M['record'].append(element)
+            return sys.getsizeof(self.children) + sum(sys.getsizeof(x) for x in self.keys)
 
-        return KM, M
-    
-    else:
-        N['record'].append(element)
-        N['record'].sort(key=lambda x: x.get('getKey', 0))
-
-        return None, None
-def insert(tree, pag,element):
-    if pag['sheet']:
-        cha, pag = insert_sheet(N=pag, element=element, size_page=args.size_page)
-        return cha, pag
-    else:
-        F = pag['search_children'](element['getKey']())
-        key , children_right = insert(tree,F, element)
-    
-    if key is not None and children_right is not None:
-        if not pag['is_full']():
-            aux = -1 
-            for x in pag['key']:
-                aux += 1
-                if key < x:
-                    pag['key'].insert(aux, key)
-                    pag['children'].insert(aux + 1, children_right)
-                    return None, None
-            pag['key'].insert(aux + 1, key)
-            pag['children'].insert(aux + 2, children_right)
-            return None, None
-        
+    def is_empty(self):
+        if self.is_leaf():
+            return not self.records
         else:
-            if len(pag['key']) % 2 == 0:
-                metade = (len(pag['children']) // 2) + 1
+            return not self.keys
+
+    def is_full(self):
+        if not self.is_empty():
+            if self.is_leaf():
+                return (self.page_size - self.page_size()) < sys.getsizeof(self.records[0])
             else:
-                metade = len(pag['children']) // 2
+                free_space = self.page_size() - self.page_size()
+                return free_space <= (sys.getsizeof(self.keys) / len(self.keys)) + sys.getsizeof(self.children) / len(self.children)
 
-            M = {'sheet': False, 'size_page': tree['size_page']}
-            M['key'] = pag['key'][(len(pag['key']) // 2):]
-            KM = M['key'].pop(0)
-            M['children'] = pag['children'][metade:]
-            pag['key'] = pag['key'][:(len(pag['key']) // 2)]
-            pag['children'] = pag['children'][:metade]
+    def find_child(self, key):
+        if not self.is_leaf():
+            pos = -1
+            for x in self.keys:
+                pos += 1
+                if key < x:
+                    return self.children[pos]
+            return self.children[pos + 1]
+        else:
+            return None
+
+    def insert_in_page(self, element):
+        if self.is_leaf():
+            if not self.is_full():
+                self.records.append(element)
+                self.records.sort(key=lambda x: x.get_key())
+
+
+class BPlusTree:
+    def __init__(self, page_size=PAGE_SIZE_DEFAULT):
+        self.root = Page(leaf=True, page_size=page_size)
+        self.page_size = page_size
+
+    def search_node(self, page, k):
+        if page.is_leaf():
+            return page
+        return self.search_node(page.find_child(k), k)
+
+    def search_range(self, k1, k2):
+        page = self.search_node(self.root, k1)
+        outside_range = False
+        interval_list = []
+
+        while not outside_range:
+            for x in page.records:
+                if k1 <= x.get_key() <= k2:
+                    interval_list.append(x.get_key())
+
+            if not outside_range:
+                page = page.right_sibling
+
+            if page is None:
+                break
+
+        return interval_list
+
+    def search_record(self, k):
+        page = self.search_node(self.root, k)
+        record = None
+        for x in page.records:
+            if x.get_key() == k:
+                record = x
+        return record, page
+
+    def insert_leaf(self, N, element):
+        if len(N.records) >= self.page_size:
+            M = Page(leaf=True, page_size=self.page_size)
+            M.records = N.records[(len(N.records) // 2):]
+            N.records = N.records[:(len(N.records) // 2)]
+            M.right_sibling = None
+            M.left_sibling = None
+            KM = M.records[0].get_key()
+
+            if element.get_key() < KM:
+                N.records.append(element)
+            else:
+                M.records.append(element)
 
             return KM, M
-    return None, None
+        else:
+            N.records.append(element)
+            N.records.sort(key=lambda x: x.get_key())
 
-def insert_source(tree, element):
-    cha, pag = insert(tree, pag=tree ['source'], element=element)
-    
-    if cha is not None and pag is not None:
-        new_source = {'sheet': False, 'size_page': tree['size_page']}
-        new_source['key'] = [cha]
-        new_source['clildren'] = [tree['source'], pag]
-        tree['source'] =  new_source
-        print("Nova raiz Criada\n")
+            return None, None
 
+    def insert(self, page, element):
+        if page.is_leaf():
+            key, new_page = self.insert_leaf(page, element)
+            return key, new_page
+        else:
+            child = page.find_child(element.get_key())
+            key, right_child = self.insert(child, element)
 
-def get_arguments(print_help=False):  
+        if key is not None and right_child is not None:
+            if not page.is_full():
+                pos = -1
+                for x in page.keys:
+                    pos += 1
+                    if key < x:
+                        page.keys.insert(pos, key)
+                        page.children.insert(pos + 1, right_child)
+                        return None, None
 
-    parser = argparse.ArgumentParser('BMais')
-    parser.add_argument('-tp', '--size_page', action='store', type=int,
-                        default=size_page,
-                        help='Maximum page size (default: ' +
-                             str(size_page) + ')')
-    parser.add_argument('-f', '--filename', action='store', type=str,
-                        default=file,
-                        help='Input filename (default: ' +
-                             file + ')')
-    args = parser.parse_args()
-    if print_help:
-        parser.print_help()
-    return args
+                page.keys.insert(pos + 1, key)
+                page.children.insert(pos + 2, right_child)
+                return None, None
+            else:
+                mid = len(page.children) // 2 + 1 if len(page.keys) % 2 == 0 else len(page.children) // 2
+
+                M = Page(leaf=False, page_size=self.page_size)
+                M.keys = page.keys[(len(page.keys) // 2):]
+                KM = M.keys.pop(0)
+                M.children = page.children[mid:]
+                page.keys = page.keys[:(len(page.keys) // 2)]
+                page.children = page.children[:mid]
+
+                return KM, M
+
+        return None, None
+
+    def insert_root(self, element):
+        key, new_page = self.insert(self.root, element)
+
+        if key is not None and new_page is not None:
+            new_root = Page(leaf=False, page_size=self.page_size)
+            new_root.keys = [key]
+            new_root.children = [self.root, new_page]
+            self.root = new_root
+
 
 
 if __name__ == '__main__':
+    
+
+    def get_arguments(print_help=False):
+        parser = argparse.ArgumentParser('BPlusTree')
+        parser.add_argument('-ps', '--page_size', action='store', type=int,
+                            default=PAGE_SIZE_DEFAULT,
+                            help='Maximum page size (default: ' +
+                                 str(PAGE_SIZE_DEFAULT) + ')')
+        parser.add_argument('-f', '--filename', action='store', type=str,
+                            default=FILE_DEFAULT,
+                            help='Input filename (default: ' +
+                                 FILE_DEFAULT + ')')
+        args = parser.parse_args()
+        if print_help:
+            parser.print_help()
+        return args
+
     args = get_arguments()
-    file = open(args.filename)
-    dados = csv.DictReader(file)
-    Bmais = {'raiz': create_page(sheet==True, size_page=args.size_page)}
+    with open(args.filename) as FILE_DEFAULT:
+        data = csv.DictReader(FILE_DEFAULT)
+        b_plus_tree = BPlusTree(page_size=args.page_size)
 
-    quantidade = 0
-    for data in dados:
-        operacao = list(data.values())
-        if operacao[0] == "+":
-            quantidade += 1
-            aux = [int(a) for a in operacao[1:]]
-            aux = {'record': [aux]}
-            cha, pag = insert(Bmais, pag=Bmais['raiz'], element=aux)
+        count = 0
+        for row in data:
+            operation = list(row.values())
+            if operation[0] == "+":
+                count += 1
+                aux = [int(a) for a in operation[1:]]
+                aux = Record(fields=aux)
+                b_plus_tree.insert_root(aux)
 
-            if quantidade == 1072:
-                print(quantidade)
-                
-    end_time = time.time()
-    
-    elapsed_time = end_time - time
-    
-    print('As operações demoraram {} segundos'.format(elapsed_time))
+                if count == 1072:
+                    print(count)
 
-    print("\nArvore criada com sucesso!Selecione uma alternativa do menu: ")
+    print("\nTree successfully created! Select an option from the menu:")
 
     while True:
-        
-        entrada = int(input(
-            "---Menu:---\n1) Buscar Elemento.\n2) Buscar intervalo.\n3) Mostrar árvore.\n4) Sair.\n"))
-        if entrada == 1:
-            chave_busca = int(input("Digite a chave que deseja buscar: "))
-            resultado, pagina = serach_record(Bmais, chave_busca)
-            if resultado:
-                print("Registro encontrado: ", serach_record(Bmais, chave_busca) )
+        choice = int(input(
+            "---Menu:---\n1) Search Element.\n2) Search Range.\n3) Show Tree.\n4) Exit.\n"))
+        if choice == 1:
+            search_key = int(input("Enter the key to search: "))
+            result, page = b_plus_tree.search_record(search_key)
+            if result:
+                print("Record found: ", result.format())
             else:
-                print("Elemento não encontrado.")
-        elif entrada == 2:
-            k1 = int(input("Digite o valor inicial do intervalo: "))
-            k2 = int(input("Digite o valor final do intervalo: "))
-            resultados = search_interval(Bmais, k1, k2)
-            if resultados:
-                print("Elementos no intervalo: ", search_interval(Bmais, k1, k2))
+                print("Element not found.")
+        elif choice == 2:
+            k1 = int(input("Enter the start value of the range: "))
+            k2 = int(input("Enter the end value of the range: "))
+            results = b_plus_tree.search_range(k1, k2)
+            if results:
+                print("Elements in the range: ", results)
             else:
-                print("Nenhum elemento encontrado no intervalo.")
-        elif entrada == 3:
-            print("\n------------ B+ -------------\n\n", Bmais['raiz'])
-        elif entrada == 4:
+                print("No elements found in the range.")
+        elif choice == 3:
+            print("\n------------ B+ Tree -------------\n\n", b_plus_tree.root)
+        elif choice == 4:
             break
         else:
-            print("Tentativa invalida! Tente novamente!")
+            print("Invalid attempt! Try again!")
